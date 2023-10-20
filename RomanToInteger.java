e08c7a3a50772649065a508116168d0e9b7e8a17
Md Noorul Islam <mdnoorul04@gmail.com>
2023-10-20 21:58:16 +0530

    Create RomanToInteger.java
    Enter a Roman Number as input and convert it to an integer.

---
+++ RomanToInteger.java
@@ -0,0 +1,56 @@
+//2. Enter a Roman Number as input and convert it to an integer.
+
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char currentChar = s.charAt(i);
+            int currentValue = getValue(currentChar);
+
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+
+    public static int getValue(char romanChar) {
+        switch (romanChar) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase(); // Convert to uppercase for case-insensitivity
+        scanner.close();
+
+        int integerEquivalent = romanToInt(romanNumeral);
+        System.out.println("Roman numeral " + romanNumeral + " is equivalent to " + integerEquivalent);
+    }
+}
